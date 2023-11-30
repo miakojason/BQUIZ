9f99b9ef9a2b275529917a682304cf7815b0a74d
miakojason <miakojason@gmail.com>
2023-11-30 13:50:02 +0800

    第1.2.3步

---
+++ q1/view.php
@@ -0,0 +1,15 @@
+<h3 class="text-center mt-3">新增標題區圖片</h3>
+<hr>
+<form action="" method="post" enctype="multipart/form-data"></form>
+<table class="col-8 m-auto">
+    <tr>
+        <td>標題區圖片:</td>
+        <td><input type="file" name="img" id=""></td>
+    </tr>
+    <td>標題區替代文字:</td>
+    <td><input type="text" name="name" id=""></td>
+</table>
+<div class="text-center">
+    <input type="submit" value="新增">
+    <input type="reset" value="重置">
+</div>
