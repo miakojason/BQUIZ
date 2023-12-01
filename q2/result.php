b6ac12d53fca8c1b4b008ca7fffda1c136523755
miakojason <miakojason@gmail.com>
2023-12-01 15:48:47 +0800

    修改空格少打

--- q2/result.php
+++ q2/result.php
@@ -1,38 +1,36 @@
 <?php include_once "./db.php"; ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>投票結果</title>
     <link rel="stylesheet" href="../css/bootstrap.css">
 </head>
-
 <body>
     <header class="p-5">
         <h1 class="text-center">投票結果</h1>
     </header>
     <main class="container">
         <?php
-        $subject = $Que->find($_GET['id']);
+        $subject=$Que->find($_GET['id']);
         ?>
-        <h2 class="text-center"><?= $subject['text']; ?></h2>
+        <h2 class="text-center"><?=$subject['text'];?></h2>
         <ul class="list-group col-6 mx-auto">
             <?php
-            $opts = $Que->all(['subject_id' => $_GET['id']]);
-            foreach ($opts as $idx => $opt) {
-                $div=($subject['count']>0?$subject['count']:1);//當分母檢查
-                $rate=round($opt['count']/$div,3);
+            $opts=$Que->all(['subject_id'=>$_GET['id']]);
+            foreach($opts as $idx => $opt){
+                $div=($subject['count']>0)?$subject['count']:1;//當分母檢查
+                $rate= round($opt['count']/$div,3);
             ?>
                 <li class="list-group-item list-group-item-action d-flex">
                     <div class="col-6">
-                        <?= $idx + 1; ?>.
-                        <?= $opt['text']; ?>
+                        <?=$idx+1;?>.
+                        <?=$opt['text'];?>
                     </div>
                     <div class="col-6 d-flex">
-                        <div class="col-8 bg-secondary" style="width:<?=$rate*0.667*100;?>%;"></div>
-                        <div class="col-4"><?=$opt['count']?>票(<?=$rate*100;?>%)</div>
+                        <div class="col-8 bg-secondary" style="width:<?=$rate*0.667*100;?>%"></div>
+                        <div class="col-4">&nbsp;&nbsp; <?=$opt['count'];?>票(<?=$rate*100;?>%)</div>
                     </div>
                 </li>
             <?php
@@ -44,5 +42,4 @@
     <script src="../js/jquery-3.4.1.min.js"></script>
     <script src="../js/bootstrap.js"></script>
 </body>
-
 </html>
